29633e68131bed784acea4d545a19c51c6d5ce84
Lucas <lucasmuzel@gmail.com>
2019-02-21 16:34:42 -0300

    Step 6
    immediately with these comments, and our previous discussion of how previous outcomes
        do not affect future outcome possibilities, you should realize that this betting method
        offers nothing more than a quicker realization of losses or gains.

--- SimpleDice.py
+++ SimpleDice.py
@@ -67,6 +67,7 @@ def simple_bettor(funds,initial_wager,wager_count):
     plt.plot(wX,vY)
 
 def doubler_bettor(funds,initial_wager,wager_count):
+    global broke_count
     value = funds
     wager = initial_wager
     wX = []
@@ -78,70 +79,72 @@ def doubler_bettor(funds,initial_wager,wager_count):
 
     # since we'll be doubling #
     previousWagerAmount = initial_wager
+    
+     '''
+    immediately with these comments, and our previous discussion of how previous outcomes
+    do not affect future outcome possibilities, you should realize that this betting method
+    offers nothing more than a quicker realization of losses or gains.
+
+    Another way to visualize this quicker realization is actually an increase in risk.
+    This bettor will experience extremely unpredictable volatility most likely. 
+    '''
 
     while currentWager <= wager_count:
         if previousWager == 'win':
-            print( 'we won the last wager, yay!' )
+            ##print 'we won the last wager, yay!'
             if rollDice():
                 value += wager
-                print( value )
+                ##print value
                 wX.append(currentWager)
                 vY.append(value)
             else:
-                value -= wager  
+                value -= wager 
                 previousWager = 'loss'
-                print( value )
+                ##print value
                 previousWagerAmount = wager
                 wX.append(currentWager)
                 vY.append(value)
                 if value < 0:
-                    print( 'went broke after',currentWager,'bets' )
+                    ##print 'went broke after',currentWager,'bets'
+                    broke_count += 1
                     currentWager += 10000000000000000
         elif previousWager == 'loss':
-            print( 'we lost the last one, so we will be super smart & double up!' )
+            ##print 'we lost the last one, so we will be super smart & double up!'
             if rollDice():
                 wager = previousWagerAmount * 2
-                print( 'we won',wager )
+                ##print 'we won',wager
                 value += wager
-                print( value )
+                ##print value
                 wager = initial_wager
                 previousWager = 'win'
                 wX.append(currentWager)
                 vY.append(value)
             else:
                 wager = previousWagerAmount * 2
-                print( 'we lost',wager )
+                ##print 'we lost',wager
                 value -= wager
-                if value < 0:
-                    print( 'went broke after',currentWager,'bets' )
-                    currentWager += 10000000000000000
-                print( value )
+                ##print value
                 previousWager = 'loss'
                 previousWagerAmount = wager
                 wX.append(currentWager)
                 vY.append(value)
                 if value < 0:
-                    print( 'went broke after',currentWager,'bets' )
+                    ##print 'went broke after',currentWager,'bets'
                     currentWager += 10000000000000000
+                    broke_count += 1
 
         currentWager += 1
 
-    print( value )
+    ##print value
     plt.plot(wX,vY)
     
-doubler_bettor(10000,100,100)
-plt.show()
-time.sleep(555)    
-    
-# lots of wagers now....
-x = 0
+xx = 0
+broke_count = 0
 
-# start this off @ 1, then add, and increase 50 to 500, then 1000
-while x < 100:
-    simple_bettor(funds=10000,initial_wager=100,wager_count=1000)
-    x += 1
-
-
-plt.ylabel('Account Value')
-plt.xlabel('Wager Count')
+while xx < 1000:             
+    doubler_bettor(10000,100,100)
+    xx+=1
+#print 'death rate:',(broke_count/float(xx)) * 100
+#print 'survival rate:',100 - ((broke_count/float(xx)) * 100)
+plt.axhline(0, color = 'r')
 plt.show()
