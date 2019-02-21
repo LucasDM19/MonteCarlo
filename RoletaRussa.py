7b4663c492159dfa61a527f68ce062b162301037
LucasDM19 <lucasmuzel@gmail.com>
2019-02-21 20:36:17 -0300

    Por no gráfico
    Agora ploto um gráfico.

--- RoletaRussa.py
+++ RoletaRussa.py
@@ -1,6 +1,8 @@
 import random
+import matplotlib
+import matplotlib.pyplot as plt
 
-def RoletaRussaSimples(qtd_tambores=6):
+def roletaRussaSimples(qtd_tambores=6):
    resultado = random.randint(1,qtd_tambores) # De 1 a 6, incluindo ambos
    
    if resultado == 1:
@@ -10,7 +12,32 @@ def RoletaRussaSimples(qtd_tambores=6):
    else:
       print( resultado, "Click! I AM ALIVE!! ")
       return True
+
+def brincaDeRoleta(qtd_tiros=10):
+   saldo = 0 # A pessoa comeca sem grana
+   saldos = []
+   vivo = True
+   for pessoa in range(qtd_tiros):
+      print("Darei o tiro de #", pessoa, ". Bora brincar")
+      if( roletaRussaSimples(qtd_tambores=500) and vivo ):
+         saldo+=10000
+         saldos.append( saldo )
+         print("Ganhei, Uhul!")
+      else:
+         saldos.append( -1000 )
+         vivo = False # Pessoa morreu
+         print("Oh, i am DEAD!")
+   return saldos
       
-for lace in range(10) :
-   resultado = RoletaRussaSimples()
-   #print(resultado)
+def fazMonteCarlo(funcao, epocas=10, grafico=plt ):
+   wX = range(epocas)
+   for lace in range(epocas) :
+      vY = funcao(epocas) # Retorna uma simulacao completa (ciclo)
+      plt.plot(wX,vY)
+   return plt
+   
+plt = fazMonteCarlo(funcao=brincaDeRoleta, epocas=100, grafico=plt)   
+
+plt.ylabel('Saldos')
+plt.xlabel('Ciclos')
+plt.show()
