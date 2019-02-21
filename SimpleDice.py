bb9260d0a9dadf19e2e2d31b2fb9214441e5a6c4
Lucas <lucasmuzel@gmail.com>
2019-02-21 15:54:40 -0300

    Step 2
    Now we need to create a bettor.

--- SimpleDice.py
+++ SimpleDice.py
@@ -2,15 +2,46 @@
 
 import random
 
+# let us go ahead and change this to return a simple win/loss
 def rollDice():
     roll = random.randint(1,100)
-    return roll
 
-# Now, just to test our dice, let's roll the dice 100 times. 
+    if roll == 100:
+        print( roll,'roll was 100, you lose. What are the odds?! Play again!')
+        return False
+    elif roll <= 50:
+        print( roll,'roll was 1-50, you lose.')
+        return False
+    elif 100 > roll >= 50:
+        print( roll,'roll was 51-99, you win! *pretty lights flash* (play more!)')
+        return True
 
-x = 0
-while x < 100:
-    result = rollDice()
-    print(result)
-    x+=1
-    
+def simpleDiceRoller():
+   # Now, just to test our dice, let's roll the dice 100 times. 
+   x = 0
+   while x < 100:
+       result = rollDice()
+       print(result)
+       x+=1
+
+'''
+Simple bettor, betting the same amount each time.
+'''
+def simple_bettor(funds,initial_wager,wager_count):
+    value = funds
+    wager = initial_wager
+
+    currentWager = 0
+
+    while currentWager < wager_count:
+        if rollDice():
+            value += wager
+        else:
+            value -= wager
+
+        currentWager += 1
+        print( 'Funds:', value )
+
+
+
+simple_bettor(10000,100,100)       
