4874ef1c64a041601f22e81927142b38483e1ac0
Lucas David Muzel <lucasmuzel@sp.gov.br>
2020-01-20 12:55:29 -0300

    Criação de objeto para Mega Sena
    Agora um objeto toma conta dos procedimentos de sorteio dos seis números da Mega Sena. Assim será melhor para ver se está tudo bem nessa parte.

--- MegaSena.py
+++ MegaSena.py
@@ -10,6 +10,16 @@ PREMIO_QUADRA = 5000 #PREMIO_QUINA / 50
 premios = {6 : PREMIO_SENA, 5 : PREMIO_QUINA, 4 : PREMIO_QUADRA,  }
 saldo = 5000.0
 
+class SorteadorMegaSena():
+   def __init__(self):
+      QTD_BOLINHAS = 60
+      self.bolinhas_sena = list(range(1,QTD_BOLINHAS))
+   def embaralhaBolinhas(self):
+      random.shuffle(self.bolinhas_sena) # Embaralha as bolinhas
+   def sorteiaMegaSena(self):
+      QTD_SORTEIO=6
+      return bolinhas_sena[0:QTD_SORTEIO]
+
 bolinhas_sena = list(range(1,QTD_BOLINHAS))
 n_sorteio = 0 # Sorteio da semana
 anterior = [] # Sorteio anterior
@@ -19,9 +29,10 @@ mundo = MeioAmbiente(tipo_agente=AgenteApostadorMegaSena, qtd_agentes=100)   # C
 benchmark = AgenteApostadorMegaSena()
 benchmark.defineAtributos(nome="YPSJD0NBCI", min_aposta=31899) # Bizarro
 mundo._agentes.append( benchmark )
+sena = SorteadorMegaSena()
 while( not sair ):
-   random.shuffle(bolinhas_sena)   # Embaralha as bolinhas
-   sorteioMegaSena = bolinhas_sena[0:QTD_SORTEIO]   # Sao 6 numeros 
+   sena.embaralhaBolinhas() 
+   sorteioMegaSena = sena.sorteiaMegaSena()   # Sao 6 numeros 
    mundo.recebeAtualizacao(sorteioMegaSena)
    mundo.notificaNovoSorteio() # Somente para display
    #n_acertei = [x for x in sorteioMegaSena for y in anterior if x==y]
