0358cb891db095615a902ae8a722d47ccb93763d
Lucas David Muzel <lucasmuzel@sp.gov.br>
2020-01-20 13:06:37 -0300

    Retirada de constantes
    Todas as constantes foram parar em cada classe de agente.

--- MegaSena.py
+++ MegaSena.py
@@ -1,13 +1,6 @@
 import random
 from SimulaMCAgente import MeioAmbiente, AgenteApostadorMegaSena
 
-VALOR_APOSTA = 4.50
-PREMIO_SENA = 3000000 
-PREMIO_QUINA = PREMIO_SENA/100
-PREMIO_QUADRA = 5000 #PREMIO_QUINA / 50
-premios = {6 : PREMIO_SENA, 5 : PREMIO_QUINA, 4 : PREMIO_QUADRA,  }
-saldo = 5000.0
-
 class SorteadorMegaSena():
    def __init__(self):
       QTD_BOLINHAS = 60
@@ -16,11 +9,9 @@ class SorteadorMegaSena():
       random.shuffle(self.bolinhas_sena) # Embaralha as bolinhas
    def sorteiaMegaSena(self):
       QTD_SORTEIO=6
-      return bolinhas_sena[0:QTD_SORTEIO]
+      return self.bolinhas_sena[0:QTD_SORTEIO]
 
-bolinhas_sena = list(range(1,QTD_BOLINHAS))
 n_sorteio = 0 # Sorteio da semana
-anterior = [] # Sorteio anterior
 sair = False
 mundo = MeioAmbiente(tipo_agente=AgenteApostadorMegaSena, qtd_agentes=100)   # Crio mundo
 benchmark = AgenteApostadorMegaSena()
